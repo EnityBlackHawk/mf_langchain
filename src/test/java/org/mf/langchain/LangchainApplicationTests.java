b35a0d25e5354d86b09c92161a6c76f4151b8329
EntityBlackHawk <luanf2003@gmail.com>
2024-02-16 16:14:42 -0300

    Initial commit

---
+++ src/test/java/org/mf/langchain/LangchainApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mf.langchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LangchainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
