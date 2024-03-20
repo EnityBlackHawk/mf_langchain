d3e3d967580149da0a994c16be2bff685a172f56
Luan Felipe Marmentini <luanf2003@gmail.com>
2024-03-20 11:19:04 -0300

    feat: Add Fk and Pk metadata

--- src/main/java/org/mf/langchain/metadata/Column.java
+++ src/main/java/org/mf/langchain/metadata/Column.java
@@ -1,8 +1,25 @@
 package org.mf.langchain.metadata;
 
+import org.jetbrains.annotations.Nullable;
 import org.mf.langchain.util.SqlDataType;
 
-public record Column(String name, SqlDataType dataType) {
+public record Column(String name, SqlDataType dataType, Boolean isPk, @Nullable FkInfo fkInfo) {
+
+
+    public boolean isFk() {
+        return fkInfo != null;
+    }
+
+    public record FkInfo(String columnName, String pk_tableName, String pk_name){
+        @Override
+        public String toString() {
+            return columnName + " REFERENCES " + pk_tableName;
+        }
+
+        public String toStringSQL() {
+            return " REFERENCES " + pk_tableName + "(" + pk_name + ")";
+        }
+    }
 
     @Override
     public String toString() {
